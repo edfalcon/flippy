b4d09cbffe31e930efe2b2a15e2240805e9d2392
Ed Falcon <ed@thefalcons.xyz>
2024-05-09 21:03:37 -0500

    Initial Commit

---
+++ Flippy/FlippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlippyApp.swift
+//  Flippy
+//
+//  Created by Edward Falcon on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct FlippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
